c7f8975a8e3c84e7d048bd65dc0ddbe35be2790c
modbender <rameshmamathayashas@gmail.com>
2023-07-09 22:40:23 +0530

    add types

--- vite-build.config.ts
+++ vite-build.config.ts
@@ -1,12 +1,18 @@
 import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [vue({ isProduction: true })],
+  plugins: [
+    vue({ isProduction: true }),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src"),
+      entry: path.resolve(__dirname, "src/index.ts"),
       name: "Vue3Disqus",
       formats: ["es", "umd", "cjs", "iife"],
     },
